7328d443767e63f842ccb1b9f03cc05e3d98a7dc
Sean Hodges <seanhodges84@gmail.com>
2021-09-10 20:53:15 +0100

    Query the DB for the feature IDs

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 
-sam package -t .\template.yml --s3-bucket aws-sam-cli-managed-default-samclisourcebucket-1uhmeo0ymx86b --output-template-file packaged-template.yaml
+sam package -t template.yml --s3-bucket incandescent-server-deploy-bucket --output-template-file packaged-template.yaml
 sam deploy --template-file packaged-template.yaml --stack-name test-stack
+aws dynamodb list-tables
+aws dynamodb batch-write-item --request-items file://docs/device-db-test-data.json (remember to rename table first)
+
+
+
+#set($deviceRef = "home/games-room/computer/" + $input.param('featureset'))
+{
+    "TableName": "test-stack-DeviceTable-MGKXW4KKJRMR",
+    "KeyConditionExpression": "userId=:v_userId and deviceRef=:v_deviceRef",
+    "ExpressionAttributeValues": {
+        ":v_userId": {"S":"seanhodges"}, 
+        ":v_deviceRef": {"S":"$deviceRef"}
+    }
+}
--- src/handlers/sendOperation/send-operation.py
+++ src/handlers/sendOperation/send-operation.py
@@ -1,6 +1,8 @@
 import json
 import logging
 import requests
+import boto3
+from boto3.dynamodb.conditions import Key
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -10,11 +12,11 @@ class BadInputError(Exception):
 
 def lambda_handler(event, context):
     try:
-        device = event.get('device')
+        device_ref = event.get('deviceRef')
         operation = event.get('operation')
         value = event.get('value')
     
-        return toggle_switch(device, operation, value)
+        return toggle_switch(device_ref, operation, value)
     except BadInputError as e:
         api_error = {
             'errorType': e.__class__.__name__,
@@ -22,15 +24,16 @@ def lambda_handler(event, context):
         }
         raise Exception(json.dumps(api_error))
     
-def toggle_switch(device, operation, value):
-    logger.info(f'Setting {device} {operation} to {value}')
-    feature_id = device # TODO: lookup the feature from the device and operation
+def toggle_switch(device_ref, operation, value):
+    feature_id = find_feature(device_ref, operation)
+    # '5b8aa9b4d36c330fd5b4e100-320-3157332334%2B1' # TODO: lookup the feature from the device and operation
+    logger.info(f'Setting {feature_id} {operation} to {value}')
         
     if feature_id == None or len(feature_id) < 3:
         raise BadInputError(f'Invalid feature id: {feature_id}')
             
     headers = {
-        'authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiI0NmFiOWY2ZTc1OWFkZmUxOTEyYSIsImlzcyI6Imh0dHBzOi8vYXV0aC5saWdodHdhdmVyZi5jb20iLCJzdWIiOiJjNTJiNDA4MS00MTA5LTQ4MTctOTE1NS1jNmY1YjBmMjdlYWQiLCJhdWQiOiI0OWQ2N2NmZC00YzVjLTQ5MDQtYjcyNi04NWFjMzRhYmY2ODAiLCJleHAiOjE2Mjk3NjMzMzQsImlhdCI6MTYyOTcyNzMzNCwic2NvcGUiOiJsd3B1YmxpYyJ9.zdr9pqGlU46lDeYdZALY9K6LgH_A8wJwL6P2FOxSduw59ay-bCpun8NKnSqshkt_LUVYnpepm5GrhiQ_9-lr0vAN9x83EDO87SRETN-UtBnjBoMAQ2gkOHeRhyBNAaZpMOlk9huip9aiAOa7DzjYQJxgj5PA2VDCVxTLZ8T4icX6HEfcHF5-thEh-Tf1YLZImISqTpTkQjYQeqMgurWeFhgIfKJDg6U5_RHrdt7BHoLjBwTN83BD7vSmGqKv0lzwB0L2IDghbCfRfe3updCihx3nBI26TXb6bRvUOZxCi1jZXq8mdVAmaFrJ5PZO5RlxZilaXTtcdssEXIGIlyNIxS7sNAw7QBgQNw9B7yaG5KcP7LMpEeApgDw65NkhFqlIf2uknF9_Q3ZN746PAVwKeAV_KL83GvS-S1M-iAORYhav1cSNnp4ubu1kdFqQWjsdRAVfNDa8h9xwr0W3mQp0T5QGndbiQDH08IpQl4R1u7e3p8rxyZ2caD3CEJ31bH0dU6p3PCGjv0zl3yzx0fKD3c3ulkRm5SJCZWM4fPXl_ky3FBTciM9jU8cPTTPQ4GpOtNOt3KLXlhcSBlD_Ls2anB3EckciLh45VgIrHX1ile4fuzroqU_ueid95nZ-vCZee_m3j__BGyuSgfvB_9tlOHTzWNyJtRkKBJqGdqX8hEw',
+        'authorization': 'bearer eyJhbGciOiJSUzI1NiJ9.eyJqdGkiOiJhZTIxOTU1N2M0ZjI0NmYyZWQ4YSIsImlzcyI6Imh0dHBzOi8vYXV0aC5saWdodHdhdmVyZi5jb20iLCJzdWIiOiJjNTJiNDA4MS00MTA5LTQ4MTctOTE1NS1jNmY1YjBmMjdlYWQiLCJhdWQiOiI0OWQ2N2NmZC00YzVjLTQ5MDQtYjcyNi04NWFjMzRhYmY2ODAiLCJleHAiOjE2MzEzMzg4NzQsImlhdCI6MTYzMTMwMjg3NCwic2NvcGUiOiJsd3B1YmxpYyJ9.Yo_yeoxYB9bULfmzPFMhWPrBm13yCl_YEm5jTj5SBd2-pUiDV4zSMEOLis_voWjdLJaI1DIxlIJMN2nreB3x-VNRH9TOgnuqSdAScWUwBdXf9W65kiZcGl78KhFvy9Q7xDSDeV8slbtZKFOVsuK9w5SFcciUd3tQFWc7l3couqbDSP81hhT6EDh3Uvb3cCxVqJA0h5F63y_H27J-gLJ9obcXzLQWRJCZTLp4WRX-LGJi4MV1zmNDhhbBx-Vsd_g8f_YQNnMnrzw0394IoNX9DWuEMbP_-9aHuQYDnd44WQGNm_TWaQlBL2P4M-6sbekc3iW5f26npRxZBzlv_EgYE9EyRzTBQdKMdjQj-V9HvpLv9DRYa3eQBtPxZSLbO3pOnB5iHNO7fK8D71f-NPH_jhE9dPzcgz32HzQlWeCfr4D1YCIRM3Z4BlDE5xX0Y8yA5_2pqx7knMIE8DDd-oNjiYOIoUpVq5Fga443yMRzWoKgwLeOIwCz6RxQ_AS46W9AGDLtXPyM9lcxXT6y2iK1qW1rh1JYZ-djtP3BuRQsl3oEogywEzpiiCA03325u3isSsb5UxTSSbkxd_FN9KHDBVjn-lphiNMQqJTEPevZwJa3QIuqD22d8CViX9pR8TXiC0agQyaY6aRZZG-UqME0HfvYuNQJqsIvtzwhsDlWnW8',
         'content-type': 'application/json'
     }
     r = requests.post(f'https://publicapi.lightwaverf.com/v1/feature/{feature_id}', json={'value': value}, headers=headers)
@@ -42,3 +45,11 @@ def toggle_switch(device, operation, value):
         logger.error(result_body)
         raise Exception(result_body.get('message') if 'message' in result_body else '')
     return result_body
+
+def find_feature(device_ref, operation):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('test-stack-DeviceTable-MGKXW4KKJRMR')
+    response = table.query(
+        KeyConditionExpression = Key('userId').eq('seanhodges') & Key('deviceRef').eq(device_ref)
+    )
+    return response['Items'][0][operation + 'Id']
