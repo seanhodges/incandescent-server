0457fb48bf85c848a56342230fd4329ab6b54705
Sean Hodges <seanhodges84@gmail.com>
2022-02-24 20:44:34 +0000

    Improved the energy recording logic and upped the memory

--- src/handlers/recordEnergyConsumption/record-energy-consumption.py
+++ src/handlers/recordEnergyConsumption/record-energy-consumption.py
@@ -16,62 +16,69 @@ energy_usage_metric = cloudwatch.Metric('incandescent_device_energy','energy_usa
 
 def lambda_handler(event, context):
     record_energy_consumption()
-    
+
+
 def record_energy_consumption():
     access_token = secrets_manager.get_secret_value(SecretId=os.environ['AccessTokenArn']).get('SecretString')
 
     # Scan device table
     table = dynamodb.Table(os.environ['DeviceTableName'])
 
-    response = table.scan()
+    response = table.scan(
+        FilterExpression='attribute_not_exists(currentPowerId) AND attribute_not_exists(energyUsageId)',
+        ProjectionExpression='deviceRef,deviceName,currentPowerId,energyUsageId'
+    )
     devices = response['Items']
 
     while 'LastEvaluatedKey' in response:
-        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        response = table.scan(
+            FilterExpression='attribute_not_exists(currentPowerId) AND attribute_not_exists(energyUsageId)',
+            ProjectionExpression='deviceRef,deviceName,currentPowerId,energyUsageId',
+            ExclusiveStartKey=response['LastEvaluatedKey']
+        )
         devices.extend(response['Items'])
 
     for device in devices:
+        update_metrics(access_token, device)
+
+
+def update_metrics(access_token, device):
         try:
+            metric_data = []
             device_name = device['deviceName']
+            device_ref = device['deviceRef']
 
             # Update energy values
             # TODO: Store hourly/weekly/monthly/yearly reset metrics in a DB and aggregate for the metric value
             if device['currentPowerId']:
                 current_power_value = get_feature_value(access_token, device['currentPowerId'])
-                update_current_power_usage(device_name, current_power_value)
+                metric_data.append({
+                        'MetricName': current_power_usage_metric.metric_name,
+                        'Value': current_power_value,
+                        'Dimensions': [
+                            { 'Name': 'device_name', 'Value': device_name },
+                            { 'Name': 'device_ref', 'Value': device_ref }
+                        ]
+                })
+
             if device['energyUsageId']:
                 energy_usage_value = get_feature_value(access_token, device['energyUsageId'])
-                update_energy_usage(device_name, energy_usage_value)
+                metric_data.append({
+                    'MetricName': energy_usage_metric.metric_name,
+                    'Value': energy_usage_value,
+                    'Dimensions': [
+                        { 'Name': 'device_name', 'Value': device_name },
+                        { 'Name': 'device_ref', 'Value': device_ref }
+                    ]
+                })
+            
+            if len(metric_data) > 0:
+                energy_usage_metric.put_data(MetricData=metric_data)
         except ClientError:
-            logger.error(f'Server error while processing device {device["deviceName"]}', exc_info=1)
+            logger.error(f'Server error while processing device {device["deviceRef"]}', exc_info=1)
         except Exception:
-            logger.error(f'Could not process device {device["deviceName"]}', exc_info=1)
+            logger.error(f'Could not process device {device["deviceRef"]}', exc_info=1)
 
-def update_current_power_usage(device_name, current_power_value):
-    current_power_usage_metric.put_data(
-        MetricData=[
-            {
-                'MetricName': current_power_usage_metric.metric_name,
-                'Value': current_power_value,
-                'Dimensions': [
-                    { 'Name': 'device_name', 'Value': device_name }
-                ]
-            }
-        ]
-    )
-
-def update_energy_usage(device_name, energy_usage_value):
-    energy_usage_metric.put_data(
-        MetricData=[
-            {
-                'MetricName': energy_usage_metric.metric_name,
-                'Value': energy_usage_value,
-                'Dimensions': [
-                    { 'Name': 'device_name', 'Value': device_name }
-                ]
-            }
-        ]
-    )
 
 def get_feature_value(access_token, feature_id):
     logger.debug(f'Getting feature value for {feature_id}')
